a7dad455e9da4f27aa1fd008c31d6da5301ac896
Ron van Weverwijk <rvanweverwijk@xebia.com>
2016-04-18 14:49:51 +0200

    We now start with the broker topics instead of consumers. If there is no consumer for a new topic

--- src/main/scala/com/godatadriven/kafka/offset/KafkaOffsetCalculator.scala
+++ src/main/scala/com/godatadriven/kafka/offset/KafkaOffsetCalculator.scala
@@ -12,7 +12,9 @@ import resource._
 
 import scala.collection.JavaConversions._
 import scala.collection.{immutable, mutable}
-import com.typesafe.config.{ConfigFactory}
+import com.typesafe.config.ConfigFactory
+
+import scala.collection.mutable.ArrayBuffer
 
 case class PartitionInfo(leader: String)
 
@@ -32,23 +34,23 @@ object KafkaOffsetCalculator {
       zookeeper => managed(new SimpleConsumers(zookeeper)) acquireAndGet {
         simpleConsumers =>
           val topicsConsumers = getTopicConsumers(zookeeper)
-          topicsConsumers.foreach(topic => {
-            val partitionsAndLeaders: mutable.Buffer[(String, SimpleConsumer)] = getPartitionsAndLeaders(zookeeper, topic._1, simpleConsumers)
+          getTopics(zookeeper).foreach(topic => {
+            val partitionsAndLeaders: mutable.Buffer[(String, SimpleConsumer)] = getPartitionsAndLeaders(zookeeper, topic, simpleConsumers)
             partitionsAndLeaders.foreach(partitionsAndLeader => {
-              val topicAndPartition: TopicAndPartition = TopicAndPartition(topic._1, partitionsAndLeader._1.toInt)
+              val topicAndPartition: TopicAndPartition = TopicAndPartition(topic, partitionsAndLeader._1.toInt)
               val offsetResponse: OffsetResponse = partitionsAndLeader._2.getOffsetsBefore(OffsetRequest(immutable.Map(topicAndPartition -> PartitionOffsetRequestInfo(OffsetRequest.LatestTime, 1))))
 
               val logSize = offsetResponse.partitionErrorAndOffsets(topicAndPartition).offsets.headOption
               logSize match {
                 case Some(size) =>
-                  result ++= "kafka_logSize{topic=\"%s\",partition=\"%s\"} %d\n".format(topic._1, partitionsAndLeader._1, size)
-                  topic._2.foreach(topicConsumer => {
+                  result ++= "kafka_logSize{topic=\"%s\",partition=\"%s\"} %d\n".format(topic, partitionsAndLeader._1, size)
+                  topicsConsumers.getOrDefault(topic, ArrayBuffer.empty).foreach(topicConsumer => {
                     val consumerId: String = topicConsumer._2
-                    val offset: Long = getConsumerOffset(zookeeper, consumerId, topicConsumer._1, partitionsAndLeader._1)
-                    result ++= "kafka_offset{topic=\"%s\",consumer=\"%s\",partition=\"%s\"} %d\n".format(topicConsumer._1, consumerId, partitionsAndLeader._1, offset)
-                    result ++= "kafka_lag{topic=\"%s\",consumer=\"%s\",partition=\"%s\"} %d\n".format(topicConsumer._1, consumerId, partitionsAndLeader._1, size - offset)
+                    val offset: Long = getConsumerOffset(zookeeper, consumerId, topic, partitionsAndLeader._1)
+                    result ++= "kafka_offset{topic=\"%s\",consumer=\"%s\",partition=\"%s\"} %d\n".format(topic, consumerId, partitionsAndLeader._1, offset)
+                    result ++= "kafka_lag{topic=\"%s\",consumer=\"%s\",partition=\"%s\"} %d\n".format(topic, consumerId, partitionsAndLeader._1, size - offset)
                   })
-                case _ => println("No logSize found for topic: %s and partition: %s".format(topic._1, partitionsAndLeader._1))
+                case _ => println("No logSize found for topic: %s and partition: %s".format(topic, partitionsAndLeader._1))
               }
             })
           })
@@ -78,6 +80,10 @@ object KafkaOffsetCalculator {
     }
   }
 
+  def getTopics(zookeeper: ZooKeeper): util.List[String] = {
+        zookeeper.getChildren("/brokers/topics", false)
+    }
+
   def getConsumerOffset(zookeeper: ZooKeeper, group_id: String, topic: String, partition: String): Long = {
     try {
       new String(zookeeper.getData("/consumers/%s/offsets/%s/%s".format(group_id, topic, partition), false, null)).toLong
@@ -92,7 +98,7 @@ object KafkaOffsetCalculator {
     children.flatMap(id => {
       val partitionJson: String = new String(zookeeper.getData("/brokers/topics/%s/partitions/%s/state".format(topic, id), false, null))
       val partitionInfo = gson.fromJson(partitionJson, classOf[PartitionInfo])
-      val consumer: SimpleConsumer = consumers.get(partitionInfo.leader).getOrElse(null)
+      val consumer: SimpleConsumer = consumers.get(partitionInfo.leader).orNull
       if (consumer != null) {
         Some(id -> consumer)
       } else {
